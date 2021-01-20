5be0f67658adf360dd50e64795137b4900c2667d
Vitor Queiroz <vitor.queiroszti@gmail.com>
2021-01-20 00:30:22 -0300

    chore: change url database

--- src/database.ts
+++ src/database.ts
@@ -2,7 +2,8 @@ import mongoose, { Mongoose } from 'mongoose';
 
 export const connect = async (): Promise<Mongoose> =>
   mongoose.connect(
-    'mongodb+srv://vitor:sala82821762@cluster0.0tokq.mongodb.net/banded?retryWrites=true&w=majority',
+    // 'mongodb+srv://vitor:sala82821762@cluster0.0tokq.mongodb.net/banded?retryWrites=true&w=majority',
+    'mongodb://localhost:27017/banded',
     {
       useCreateIndex: true,
       useNewUrlParser: true,
